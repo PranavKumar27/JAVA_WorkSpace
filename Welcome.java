641ec384bd7bed6962ea29006cfc57c61ff01f8b
Pranav Kumar <pranav.jenus@gmail.com>
2025-06-30 22:53:26 +0530

    Update Welcome.java

--- Welcome.java
+++ Welcome.java
@@ -5,7 +5,7 @@ class Welcome
 	public static void main(String args[])
 	{
 		Scanner s = new Scanner(System.in);
-		
+		System.out.println("Enter name: ");
 		String name = s.nextLine();
 		System.out.println("Welcome  "+name);
 	}
