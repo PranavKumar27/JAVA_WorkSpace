312eca80f66d1c815e46614bd80a0f4d085e44ac
Pranav Kumar <pranav.jenus@gmail.com>
2025-06-30 22:51:55 +0530

    Create Welcome.java

---
+++ Welcome.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Welcome
+{
+	public static void main(String args[])
+	{
+		Scanner s = new Scanner(System.in);
+		
+		String name = s.nextLine();
+		System.out.println("Welcome  "+name);
+	}
+}
