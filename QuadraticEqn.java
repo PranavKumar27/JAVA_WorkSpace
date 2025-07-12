0b70acfa8e583df5b83d6e795fc8818b35fb217f
unknown <pranav.jenus@gmail.com>
2025-07-13 00:03:36 +0530

    Quad Eqn

---
+++ QuadraticEqn.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class QuadraticEqn {
+    public static void main(String args[])
+    {
+        System.out.println("Finding Roots of Quadratic Eqn>>>");
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        double root1 = (-b-Math.sqrt((b*b)-(4*a*c)))/(2*a);
+        
+        double root2 = (-b+Math.sqrt((b*b)-(4*a*c)))/(2*a);
+
+        System.out.println("root 1 >>> "+root1);
+        System.out.println("root 2 >>> "+root2);
+    }
+}
