034b3ee3e489dd6c75825339506b8f59298eef32
unknown <pranav.jenus@gmail.com>
2025-07-13 20:20:56 +0530

    PreIncremen

---
+++ Arithmetic/PreIncrement.java
@@ -0,0 +1,44 @@
+
+
+public class PreIncrement {
+    public static void main(String args[])
+    {
+        int x = 5;
+        int y = x++;
+
+        print(x);
+        print(y);
+
+        x = 5;
+        int z = ++x;
+        print(x);
+        print(z);
+
+        x=5;y=4;
+        int res = 2*x++ + 3 * ++y;
+        print(res);
+
+        x=5;y=4;
+        int res2 = 2*x++ + 3 * ++x;
+        print(res2);
+
+        float f=3.5f;
+        ++f;
+        System.out.println("f = "+f);
+
+        char cx = 65;
+        ++cx;
+        print(cx);
+
+        byte mb = 10;
+        mb++;
+        // mb = mb+1;  <--- Error 
+        print(mb);
+
+    }
+    static void print(int x)
+    {
+        System.out.println("Data= "+x);
+    }
+
+}
